d7b0a870c101375ea6b6676b0359e3be0721ac12
Presbyterian Librarian <jjhembd@gmail.com>
2019-04-08 12:18:53 -0500

    Standardized directory structure and build script

--- build/glsl-plugin.js
+++ build/glsl-plugin.js
@@ -1,7 +1,8 @@
 // Plugin for .glsl files, roughly following the rollup.config.js in three.js
 // with some guidance from github.com/vwochnik/rollup-plugin-glsl
 // and rollupjs.org/guide/en#plugins-overview
-function glsl() {
+// TODO: replace with glslify and rollup-plugin-glslify ?
+export function glsl() {
   return {
     transform( source, id ) { // Too much indentation? Follows three.js...
       // Confirm filename extension is .glsl -- follows three.js
@@ -18,15 +19,3 @@ function glsl() {
     }
   };
 }
-
-export default {
-  input: 'src/yawgl.js',
-  plugins: [
-    glsl(),
-  ],
-  output: {
-    file: 'build/yawgl.bundle.js',
-    format: 'esm',
-    name: 'yawgl'
-  }
-};
